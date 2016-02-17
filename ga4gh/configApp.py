cb50b5e79f8a70fa1a0973b321a9fb1ea52bb0dc
David Steinberg <david@resium.com>
2016-02-16 19:35:03 -0800

    Move configurator to its own file
    So it may be interestingly documented

---
+++ ga4gh/configApp.py
@@ -0,0 +1,125 @@
+import os
+
+import requests
+
+import flask
+import flask.ext.cors as cors
+import urlparse
+
+import oic
+import oic.oauth2
+import oic.oic.message as message
+
+import ga4gh.backend as backend
+import ga4gh.exceptions as exceptions
+import ga4gh.datamodel as datamodel
+from serverStatus import ServerStatus
+
+import ga4gh.datarepo as datarepo
+
+SECRET_KEY_LENGTH = 24
+
+def configure(app, configFile=None, baseConfig="ProductionConfig",
+              port=8000, extraConfig={}):
+    """
+    TODO Document this critical function! What does it do? What does
+    it assume?
+    """
+    configStr = 'ga4gh.serverconfig:{0}'.format(baseConfig)
+    app.config.from_object(configStr)
+    if os.environ.get('GA4GH_CONFIGURATION') is not None:
+        app.config.from_envvar('GA4GH_CONFIGURATION')
+    if configFile is not None:
+        app.config.from_pyfile(configFile)
+    app.config.update(extraConfig.items())
+    # Setup file handle cache max size
+    datamodel.fileHandleCache.setMaxCacheSize(
+        app.config["FILE_HANDLE_CACHE_MAX_SIZE"])
+    # Setup CORS
+    cors.CORS(app, allow_headers='Content-Type')
+    app.serverStatus = ServerStatus(app)
+    # Allocate the backend
+    # We use URLs to specify the backend. Currently we have file:// URLs (or
+    # URLs with no scheme) for the FileSystemBackend, and special empty:// and
+    # simulated:// URLs for empty or simulated data sources.
+    dataSource = urlparse.urlparse(app.config["DATA_SOURCE"], "file")
+
+    if dataSource.scheme == "simulated":
+        # Ignore the query string
+        randomSeed = app.config["SIMULATED_BACKEND_RANDOM_SEED"]
+        numCalls = app.config["SIMULATED_BACKEND_NUM_CALLS"]
+        variantDensity = app.config["SIMULATED_BACKEND_VARIANT_DENSITY"]
+        numVariantSets = app.config["SIMULATED_BACKEND_NUM_VARIANT_SETS"]
+        numReferenceSets = app.config[
+            "SIMULATED_BACKEND_NUM_REFERENCE_SETS"]
+        numReferencesPerReferenceSet = app.config[
+            "SIMULATED_BACKEND_NUM_REFERENCES_PER_REFERENCE_SET"]
+        numAlignmentsPerReadGroup = app.config[
+            "SIMULATED_BACKEND_NUM_ALIGNMENTS_PER_READ_GROUP"]
+        dataRepository = datarepo.SimulatedDataRepository(
+            randomSeed=randomSeed, numCalls=numCalls,
+            variantDensity=variantDensity, numVariantSets=numVariantSets,
+            numReferenceSets=numReferenceSets,
+            numReferencesPerReferenceSet=numReferencesPerReferenceSet,
+            numAlignments=numAlignmentsPerReadGroup)
+    elif dataSource.scheme == "empty":
+        dataRepository = datarepo.EmptyDataRepository()
+    elif dataSource.scheme == "file":
+        dataRepository = datarepo.FileSystemDataRepository(os.path.join(
+            dataSource.netloc, dataSource.path))
+        dataRepository.checkConsistency()
+    else:
+        raise exceptions.ConfigurationException(
+            "Unsupported data source scheme: " + dataSource.scheme)
+    theBackend = backend.Backend(dataRepository)
+    theBackend.setRequestValidation(app.config["REQUEST_VALIDATION"])
+    theBackend.setResponseValidation(app.config["RESPONSE_VALIDATION"])
+    theBackend.setDefaultPageSize(app.config["DEFAULT_PAGE_SIZE"])
+    theBackend.setMaxResponseLength(app.config["MAX_RESPONSE_LENGTH"])
+    app.backend = theBackend
+    app.secret_key = os.urandom(SECRET_KEY_LENGTH)
+    app.oidcClient = None
+    app.tokenMap = None
+    app.myPort = port
+    if "OIDC_PROVIDER" in app.config:
+        # The oic client. If we're testing, we don't want to verify
+        # SSL certificates
+        app.oidcClient = oic.oic.Client(
+            verify_ssl=('TESTING' not in app.config))
+        app.tokenMap = {}
+        try:
+            app.oidcClient.provider_config(app.config['OIDC_PROVIDER'])
+        except requests.exceptions.ConnectionError:
+            configResponse = message.ProviderConfigurationResponse(
+                issuer=app.config['OIDC_PROVIDER'],
+                authorization_endpoint=app.config['OIDC_AUTHZ_ENDPOINT'],
+                token_endpoint=app.config['OIDC_TOKEN_ENDPOINT'],
+                revocation_endpoint=app.config['OIDC_TOKEN_REV_ENDPOINT'])
+            app.oidcClient.handle_provider_config(configResponse,
+                                                  app.config['OIDC_PROVIDER'])
+
+        # The redirect URI comes from the configuration.
+        # If we are testing, then we allow the automatic creation of a
+        # redirect uri if none is configured
+        redirectUri = app.config.get('OIDC_REDIRECT_URI')
+        if redirectUri is None and 'TESTING' in app.config:
+            redirectUri = 'https://{0}:{1}/oauth2callback'.format(
+                socket.gethostname(), app.myPort)
+        app.oidcClient.redirect_uris = [redirectUri]
+        if redirectUri is []:
+            raise exceptions.ConfigurationException(
+                'OIDC configuration requires a redirect uri')
+
+        # We only support dynamic registration while testing.
+        if ('registration_endpoint' in app.oidcClient.provider_info and
+           'TESTING' in app.config):
+            app.oidcClient.register(
+                app.oidcClient.provider_info["registration_endpoint"],
+                redirect_uris=[redirectUri])
+        else:
+            response = message.RegistrationResponse(
+                client_id=app.config['OIDC_CLIENT_ID'],
+                client_secret=app.config['OIDC_CLIENT_SECRET'],
+                redirect_uris=[redirectUri],
+                verify_ssl=False)
+            app.oidcClient.store_registration_info(response)

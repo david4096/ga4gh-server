76470994e7056735929afac5593ccba3d8cdaa47
David Steinberg <david@resium.com>
2016-12-21 10:40:49 -0800

    Multiprocess rna

--- scripts/rna_and_populations.py
+++ scripts/rna_and_populations.py
@@ -245,19 +245,45 @@ def main():
     store = rnaseq2ga.RnaSqliteStore(quant_location)
     store.createTables()
     directory_contents = os.listdir(kallisto_location)
+
+    rnadirs = mp.Queue()
+    rnadone = mp.Queue()
+    workers = 3
+    processes = []
+
+
+    def add_quant(todo, done):
+        directory = todo.get()
+        while directory is not None:
+            print("Adding RNA for {}".format(directory))
+            filename_location = os.path.join(kallisto_location, directory, 'abundance.tsv')
+            rnaseq2ga.rnaseq2ga(
+                filename_location, quant_location, directory,
+                'kallisto', dataset=dataset, featureType='transcript',
+                description='RNA seq data from lymphoblastoid cell lines in the 1000 Genome Project, '
+                                 'http://www.ebi.ac.uk/arrayexpress/experiments/E-GEUV-1/samples/',
+                programs=None,
+                featureSetNames='gencode_v24lift37',
+                readGroupSetNames=None,
+                bioSampleId=dataset.getBioSampleByName(directory).getLocalId())
+            done.put(directory)
+            directory = todo.get()
+        else:
+            return
+
     for directory in directory_contents:
-        if directory is not None:
-          print("Adding RNA for {}".format(directory))
-          filename_location = os.path.join(kallisto_location, directory, 'abundance.tsv')
-          rnaseq2ga.rnaseq2ga(
-            filename_location, quant_location, directory,
-            'kallisto', dataset=dataset, featureType='transcript',
-            description='RNA seq data from lymphoblastoid cell lines in the 1000 Genome Project, ' 
-                             'http://www.ebi.ac.uk/arrayexpress/experiments/E-GEUV-1/samples/', 
-            programs=None,
-            featureSetNames='gencode_v24lift37',
-            readGroupSetNames=None,
-            bioSampleId=dataset.getBioSampleByName(directory).getLocalId())
+        rnadirs.put(directory)
+    for w in xrange(workers):
+        p = mp.Process(target=add_quant, args=(rnadirs, rnadone))
+        p.start()
+        processes.append(p)
+        rnadirs.put(None) # Stop sentinel
+    for p in processes:
+        p.join()
+    x = rnadone.get()
+    while x:
+        print ("Loaded {}".format(x))
+        x = rnadone.get()
     rnaQuantificationSet = rna_quantification.SqliteRnaQuantificationSet(dataset, "E-GEUV-1 RNA Quantification")
     rnaQuantificationSet.setReferenceSet(gencode)
     rnaQuantificationSet.populateFromFile(quant_location)
